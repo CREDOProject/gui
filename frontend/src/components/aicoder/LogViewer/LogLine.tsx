32d079c4de0d228c99c99965357182977a2f6997
Eliseo Martelli <me@eliseomartelli.it>
2025-11-07 10:39:33 +0100

    fix: remove unused files

--- frontend/src/components/aicoder/LogViewer/LogLine.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export const LogLine = ({ index, log }: { index: number; log: string }) => (
-  <p>
-    <span className="w-10 bg-gray-100 inline-block text-end">{index}</span>{" "}
-    {log}
-  </p>
-);
